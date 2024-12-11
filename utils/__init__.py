bf508be6d3ed6d6baad000de3bab8f7294b41814
coreyfellows <corey@fello.ws>
2024-12-11 12:16:10 -0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,12 @@
 
 from itertools import chain
+from typing import Generic, Callable
+from collections.abc import Iterable
 
-class Grid:
-    def copy(self):
-        grid = []
-        for line in self.grid:
-            grid.append([e.copy() for e in line])
-        return Grid(grid)
-    
-    def __init__(self, fp, cls):
+T = TypeVar("T", bound=Callable[[str]])
+
+class Grid(Generic[T]):    
+    def __init__(self, fp, cls: T):
         lines = [l.strip() for l in fp.readlines()]
         grid = []
         for line in lines:
@@ -21,7 +19,7 @@ class Grid:
         self.width = len(grid)
         self.height = len(grid[0])
     
-    def __getitem__(self, coords):
+    def __getitem__(self, coords) -> T:
         x,y=coords
         if x < 0 or x >= self.width:
             return None
@@ -29,12 +27,12 @@ class Grid:
             return None
         return self.grid[y][x]
     
-    def __iter__(self):
+    def __iter__(self) -> Iterable[tuple[T, int, int]:
         for y in range(self.height):
             for x in range(self.width):
                 yield self[y,x], y, x
 
-    def __str__(self):
+    def __str__(self) -> str:
         lines = []
         for y in range(self.height):
             line = []
@@ -43,16 +41,16 @@ class Grid:
             lines.append("".join(line))
         return "\n".join(lines)
 
-    def select_neighbors(self, x, y, relative_positions):
+    def select_neighbors(self, x, y, relative_positions) -> Iterable[tuple[T, int, int]:
         for dx, dy in relative_positions:
             if self[x+dx, y+dy]:
                 yield self[x+dx, y+dy], x+dx, y+dy
 
-    def cardinal_neighbors(self, x, y):
+    def cardinal_neighbors(self, x, y) -> Iterable[tuple[T, int, int]:
         return self.select_neighbors(x,y, [(1,0), (0,1), (-1,0), (0,-1)])
     
-    def diagonal_neighbors(self, x, y):
+    def diagonal_neighbors(self, x, y) -> Iterable[tuple[T, int, int]:
         return self.select_neighbors(x,y, [(1,1), (-1,-1), (-1,1), (1,-1)])
     
-    def neighbors(self, x ,y):
+    def neighbors(self, x ,y) -> Iterable[tuple[T, int, int]:
         return chain(self.cardinal_neighbors(x,y), self.diagonal_neighbors(x,y))
